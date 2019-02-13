34a92565b79c71838ba28ed312e5226f063c84f3
huangpeidong <602041937@qq.com>
2019-02-13 11:15:17 +0800

    初始化项目

---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'hpd-ios-mvvm' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for hpd-ios-mvvm
+    pod 'RxSwift',    '~> 4.0'
+    pod 'RxCocoa',    '~> 4.0'
+    pod 'Action'
+
+end
---
+++ hpd-ios-mvvm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  hpd-ios-mvvm
+//
+//  Created by huangpeidong on 2019/2/13.
+//  Copyright © 2019年 huangpeidong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
